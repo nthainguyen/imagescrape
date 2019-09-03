8c877f02d766f93098630b9080fd4061bd48f9a4
hai nguyen <nguyentienhai@hotmail.com.vn>
2019-09-03 15:20:02 +0700

    first commit

---
+++ crawl1/crawl1/spiders/spiderone.py
@@ -0,0 +1,37 @@
+import scrapy
+import json
+import wget
+import requests
+class Crawler(scrapy.Spider):
+    name = "spiderone"
+    start_urls = [
+        'https://digitalcollections.nypl.org/search/index?filters%5BphysicalLocation_mtxt_s%5D%5B%5D=Map+Division&format=html&keywords=&per_page=250&page=1',
+    ]
+
+    def parse(self, response):
+        pattern = r'\bvar\s+search_results\s*=\s*(\[.*?\])\s*;\s*\n'
+        json_data = response.css('script::text').re_first(pattern)
+        print(json_data)
+        maps = json.loads(json_data)
+        for map in maps:
+            yield scrapy.Request('https://digitalcollections.nypl.org/items/' + map['item']['id'],\
+                 callback= self.parse_item)
+
+        next_page_url = response.css(".next_page::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
+
+    def parse_item(self, response):
+        data_items = response.css('div.carousel-widget::attr(data_items)').get()
+        if data_items:
+            sub_imgs = json.loads(data_items)
+            for img in sub_imgs:
+                yield img['high_res_link']
+        else:
+            original = response.css('div.original > a::attr(href)').get()
+            r = requests.get(original)
+            with open(original[::-15], 'wb') as f:
+                f.write(r.content)
+            yield {
+                'id': original
+            }
