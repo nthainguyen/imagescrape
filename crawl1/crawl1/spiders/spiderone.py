dfbc01866a60f1eb3748cd73df625d5bd93fe06a
nguyentienhai <nguyentienhai@hotmail.com.vn>
2019-09-08 21:14:31 +0700

    finished

--- crawl1/crawl1/spiders/spiderone.py
+++ crawl1/crawl1/spiders/spiderone.py
@@ -11,7 +11,6 @@ class Crawler(scrapy.Spider):
     def parse(self, response):
         pattern = r'\bvar\s+search_results\s*=\s*(\[.*?\])\s*;\s*\n'
         json_data = response.css('script::text').re_first(pattern)
-        print(json_data)
         maps = json.loads(json_data)
         for map in maps:
             yield scrapy.Request('https://digitalcollections.nypl.org/items/' + map['item']['id'],\
@@ -22,15 +21,29 @@ class Crawler(scrapy.Spider):
             yield scrapy.Request(response.urljoin(next_page_url))
 
     def parse_item(self, response):
-        data_items = response.css('div#item-carousel::attr(data-items)').get()
-        if data_items:
-            data = json.loads(data_items)
-            for i in data:
-                yield {
-                    'links': i['high_res_link']
-                }
-        else:
-            original = response.css('div.original > a::attr(href)').get()
-            yield {
-                'id': original
-            }
+    	if response.status == 404:
+    		yield {
+    			'src': response.url,
+    			'links': 'Error 404 - This page does not exist'
+    		}
+    	else:
+		    data_items = response.css('div#item-carousel::attr(data-items)').get()
+		    if data_items:
+		        original_link = response.css('div.original > a::attr(href)').get()
+		        data = json.loads(data_items)
+		        for i in data:
+		            yield {
+		            	'image_id': i['image_id'],
+		            	'title': i['title'],
+		            	'src': response.url,
+		                'links': "https://images.nypl.org/index.php?id={0}&t=g&download=1".format(i['image_id'])
+		            }
+		    else:
+		        data_item = response.css('div#bigimage > img::attr(data-item)').get()
+		        data = json.loads(data_item)
+		        yield {
+		        	'image_id': data['image_id'],
+		        	'title': data['title'],
+		        	'src': response.url,
+		            'links': "https://images.nypl.org/index.php?id={0}&t=g&download=1".format(data['image_id'])
+		        }
